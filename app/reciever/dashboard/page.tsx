10b5e9895c0201f2049cdcbd30888d3cae7436b7
Anii <aniruddh.nagare@vpt.edu.in>
2024-03-21 12:25:44 +0530

    dashboard apos

--- app/reciever/dashboard/page.tsx
+++ app/reciever/dashboard/page.tsx
@@ -89,8 +89,7 @@ export default function RecieverDashboard() {
         </Button>
       </div>
       <div className="flex flex-col gap-5  w-full" suppressHydrationWarning>
-        <PageTitle title="Dashboard" />
-
+        
         <section className="grid w-full grid-cols-1 gap-4 gap-x-8 transition-all sm:grid-cols-2 xl:grid-cols-4">
           {cardData.map(
             (

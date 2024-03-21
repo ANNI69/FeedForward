12a7e47e97aa5d24d0941307ea3ca418c2432e14
Anii <aniruddh.nagare@vpt.edu.in>
2024-03-21 15:39:02 +0530

    Done solving the error

--- app/reciever/dashboard/page.tsx
+++ app/reciever/dashboard/page.tsx
@@ -20,25 +20,21 @@ const cardData: CardProps[] = [
   {
     label: "Total Revenue",
     amount: "$45,231.89",
-    discription: "+20.1% from last month",
     icon: DollarSign,
   },
   {
     label: "Subscriptions",
     amount: "+2350",
-    discription: "+180.1% from last month",
     icon: Users,
   },
   {
     label: "Sales",
     amount: "+12,234",
-    discription: "+19% from last month",
     icon: CreditCard,
   },
   {
     label: "Active Now",
     amount: "+573",
-    discription: "+201 since last hour",
     icon: Activity,
   },
 ];
@@ -94,7 +90,6 @@ export default function RecieverDashboard() {
             (
               d: {
                 amount: string;
-                discription: string;
                 icon: LucideIcon;
                 label: string;
               },
@@ -103,7 +98,6 @@ export default function RecieverDashboard() {
               <Card
                 key={i}
                 amount={d.amount}
-                discription={d.discription}
                 icon={d.icon}
                 label={d.label}
               />

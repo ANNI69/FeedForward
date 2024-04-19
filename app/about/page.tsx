8304c2ee178b04f09cb5ea2e7f8fb576580ef001
Anii <aniruddh.nagare@vpt.edu.in>
2024-04-19 13:58:45 +0530

    Changes

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -6,19 +6,26 @@ import {
   AccordionItem,
   AccordionTrigger,
 } from "@/components/ui/accordion";
+import { Nav } from "@/components/ui/nav";
+import Navbar from "@/components/navbar";
 
 const words = `At FeedForward, we're driven by the vision of a sustainable world where every action and every innovation contributes to a healthier planet. Born from a passion to tackle some of the most pressing environmental challenges of our time, our platform serves as a dynamic bridge connecting NGOs, Food Processors and Many Composting and Biogas Plants with opportunities to make a tangible impact`;
 
 export default function About() {
   return (
     <>
-      <div className="m-10 p-10 justify-center ">
-        <div className="m-20 h-[80vh]">
+      <Navbar />
+      <div className="p-10">
+        <div className="h-[80vh]">
           <TextGenerateEffect words={words} />
         </div>
 
-        <div className="mt-20 ">
-          <Accordion type="single" collapsible className="w-full h-full">
+        <div className="mt-20">
+          <Accordion
+            type="single"
+            collapsible
+            className=" ml-15 mr-15 w-full h-full"
+          >
             <AccordionItem value="item-1">
               <AccordionTrigger>
                 How does FeedForward contribute to environmental sustainability?
--- app/home/page.tsx
+++ app/home/page.tsx
@@ -11,7 +11,6 @@ export default function Homepage() {
   const images = [
     "https://info.ehl.edu/hubfs/Blog-EHL-Insights/Blog-Header-EHL-Insights/zero-food-waste.jpg",
   ];
-
   return (
     <>
       <Navbar />
@@ -30,8 +29,9 @@ export default function Homepage() {
           }}
           className="z-50 flex flex-col justify-center items-center"
         >
-          <motion.p className="font-bold text-xl md:text-8xl text-cente bg-clip-text text-transparent text-center bg-gradient-to-b from-neutral-50 to-neutral-400 py-4 font-[Nova]">
-            <br /> Turning Leftovers into Opportunities
+          <motion.p className="font-bold text-xl md:text-8xl text-cente bg-clip-text text-transparent text-center bg-gradient-to-b from-neutral-50 to-neutral-400 py-4 font-[Nova] sm:text-xl">
+            Turning Leftovers <br />
+            into Opportunities
           </motion.p>
           <button className="px-10 py-4 backdrop-blur-sm border bg-emerald-300/10 border-emerald-500/20 text-white mx-auto text-center rounded-full relative mt-4">
             <span>
@@ -44,13 +44,13 @@ export default function Homepage() {
       </ImagesSlider>
       <div className="h-[30vh] w-full rounded-md bg-black relative flex flex-col items-center justify-center antialiased">
         <div className="max-w-2xl mx-auto p-4">
-          <h2 className="relative z-10 text-lg md:text-7xl  bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-600  text-center font-sans font-bold">
+          <h2 className="relative z-10 text-lg md:text-7xl pt-20 bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-600  text-center font-sans font-bold">
             Donate From Here.
           </h2>
         </div>
         <BackgroundBeams />
       </div>
-      <div className=" bg-black flex flex-wrap justify-center space-x-2 ml-5 mr-5 gap-10">
+      <div className=" bg-black flex flex-wrap justify-center space-x-2 pt-20 gap-10">
         {cardsData &&
           cardsData.map((value, index) => {
             return <MeteorsDemo key={index} {...value} />;

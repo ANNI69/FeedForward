2c465f47891c79c7f0e1c006324136f27cb3e18f
Aniruddh Nagare <84516362+ANNI69@users.noreply.github.com>
2024-07-12 23:40:04 +0530

    changes

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 export const connectDB = async () => {
   try {
     await mongoose.connect(
-      "mongodb+srv://shivamnarkar16:Shivam1111@cluster0.0ulcwjv.mongodb.net/feedforward?retryWrites=true&w=majority&appName=Cluster0"
+      "mongodb+srv://codebyanii:codebyanii@cluster0.jzioxnh.mongodb.net"
     );
     console.log("Connected to MongoDB");
   } catch (e) {

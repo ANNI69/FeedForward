c16743331f9ce303b2ca1502246681d5df5c43e4
Aniruddh Nagare <84516362+ANNI69@users.noreply.github.com>
2024-07-13 19:51:01 +0530

    "use client"; 2

--- Frontend/my-app/src/components/ModeSwitch.tsx
+++ Frontend/my-app/src/components/ModeSwitch.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { Button } from "./ui/button";
 import { MoonIcon, SunIcon } from "@radix-ui/react-icons";
 import { useTheme } from "next-themes";
--- Frontend/my-app/src/components/ui/meteors.tsx
+++ Frontend/my-app/src/components/ui/meteors.tsx
@@ -1,3 +1,4 @@
+"use client"; 
 import { cn } from "@/lib/utils";
 import clsx from "clsx";
 import React from "react";
